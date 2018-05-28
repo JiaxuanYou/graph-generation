25db420b3bcf93f17c64c3c44e391c5f364aa2e8
JiaxuanYou <youjiaxuan@gmail.com>
2018-05-28 14:12:01 -0700

    clean import

--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-import networkx as nx
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.init as init
-from torch.autograd import Variable
-import matplotlib.pyplot as plt
-import torch.nn.functional as F
-from torch import optim
-from torch.optim.lr_scheduler import MultiStepLR
-from sklearn.decomposition import PCA
-import logging
-from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
-from time import gmtime, strftime
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import average_precision_score
-from random import shuffle
-import pickle
-from tensorboard_logger import configure, log_value
-import scipy.misc
-import time as tm
-from utils import *
-from model import *
-from data import *
-
-
-
-from args import Args
-import create_graphs
 from train import *
 
 if __name__ == '__main__':
